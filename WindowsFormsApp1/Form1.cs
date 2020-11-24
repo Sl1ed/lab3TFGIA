0651e34166bb4d4b6a54e94a58a1d2d4fab84218
Sl1ed <45339767+Sl1ed@users.noreply.github.com>
2020-11-24 23:39:34 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tbFSource.AppendText("adc:=[000101110]" + "\r\n");
+            int n = tbFSource.Lines.Length;
+        }
+
+        private void btnFStart_Click(object sender, EventArgs e)
+        {
+            tbFMessage.Clear();
+            uSyntAnalyzer Synt = new uSyntAnalyzer();
+            Synt.Lex.strPSource = tbFSource.Lines;
+            Synt.Lex.strPMessage = tbFMessage.Lines;
+            Synt.Lex.enumPState = TState.Start;
+            try
+            {
+                Synt.Lex.NextToken();
+                Synt.S();
+                throw new Exception("Текст верный");
+            }
+            catch (Exception exc)
+            {
+                tbFMessage.Text += exc.Message;
+                tbFSource.Select();
+                tbFSource.SelectionStart = 0;
+                int n = 0;
+                for (int i = 0; i < Synt.Lex.intPSourceRowSelection; i++) n += tbFSource.Lines[i].Length + 2;
+                n += Synt.Lex.intPSourceColSelection;
+                tbFSource.SelectionLength = n;
+
+
+            }
+        }
+
+     
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ WindowsFormsApp1/uSyntAnalyzer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class uSyntAnalyzer
+    {
+        private String[] strFSource;
+        private String[] strFMessage;
+        public String[] strPSource { set { strFSource = value; } get { return strFSource; } }
+        public String[] strPMessage { set { strFMessage = value; } get { return strFMessage; } }
+        public CLex Lex = new CLex();
+
+        public void S()
+        {
+            A();
+            if (Lex.enumPToken == TToken.lxmtz)
+                C();
+            throw new Exception("Конец слова, текст верный. Для продолжения ожидается ;");
+        }
+        public void C()
+        {
+            if (Lex.enumPToken == TToken.lxmtz)
+            {
+                Lex.NextToken();
+                A();
+                if (Lex.enumPToken == TToken.lxmtz)
+                    C();
+            }
+        }
+        public void A()
+        {
+            if (Lex.enumPToken == TToken.lxmIdentifier)
+            {
+                Lex.NextToken();
+                if (Lex.enumPToken == TToken.lxmdt)
+                {
+                    Lex.NextToken();
+                    if (Lex.enumPToken == TToken.lxmr)
+                    {
+                        Lex.NextToken();
+                        if (Lex.enumPToken == TToken.lxmls)
+                        {
+                            Lex.NextToken();
+                            B();
+
+                            if (Lex.enumPToken == TToken.lxmrs)
+                            {
+                                Lex.NextToken();
+                            }
+                            else throw new Exception("Ожидалась , или ]");
+                        }
+                        else throw new Exception("Ожидалась [");
+                    }
+                    else throw new Exception("Ожидалось =");
+                }
+                else throw new Exception("Ожидалось :");
+            }
+            else throw new Exception("Ожидался идентификатор");
+        }
+        public void B()
+        {
+            if (Lex.enumPToken == TToken.lxmNumber)
+            {
+                Lex.NextToken();
+                if (Lex.enumPToken == TToken.lxmComma)
+                {
+                    Lex.NextToken();
+                    B();
+                }
+            }
+            else throw new Exception("Ожидался идентификатор");
+        }
+    }
+
+}
